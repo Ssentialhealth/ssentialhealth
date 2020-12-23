947567109e42eb588af930befe1166f8b252ceb3
Kokodavid <kingkgdave@gmail.com>
2020-12-23 12:07:58 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/pocket/pocket_health/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pocket.pocket_health
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:pocket_health/screens/loginScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pocket Health',
+      theme: ThemeData(
+   
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen()
+    );
+  }
+}
+
+
+
+
