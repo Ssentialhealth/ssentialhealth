b4bc88a0ac6c293504f2c66aa97ca238ed0ed4d9
Kokodavid <kingkgdave@gmail.com>
2021-01-13 19:05:11 +0300

    code CleanUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:pocket_health/screens/loginScreen.dart';
 import 'package:pocket_health/screens/forgot_password.dart';
 import 'package:pocket_health/screens/home_screen.dart';
 import 'package:pocket_health/screens/sign_in_screen.dart';
-import 'package:pocket_health/screens/signup_screen.dart';
 import 'package:pocket_health/services/auth_service.dart';
 
-import 'providers/form_provider.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -23,12 +21,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xff1F1F1F)
       ),
       home: SignInScreen(),
-      routes: {
-        '/home': (_) => HomeScreen(),
-        '/login': (_) => Login(),
-        '/signUp': (_) => SignUpScreen(),
-        '/forgotPassword': (_) => ForgotPassword(),
-      },
+
     );
   }
 
