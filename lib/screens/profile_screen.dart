4f4ec34446b11be90b7431931b8b0596307ada20
Kokodavid <kingkgdave@gmail.com>
2021-01-29 14:01:08 +0300

    Profile Screen UI

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,1 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_health/widgets/widget.dart';
+
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+        backgroundColor: Color(0xFF00FFFF),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Container(
+          child: Row(
+            children: <Widget>[
+              Container(
+                color: Color(0xFF00FFFF),
+                height: 90,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Container(
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(30),
+                            child: Image.asset("assets/images/download.png", height: 50,)),
+                      ),
+                    ),
+                    Text("Nicholas Dani",style: mediumTextStyle(),),
+                    Spacer(flex: 2,),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(Icons.arrow_forward_ios_outlined),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
